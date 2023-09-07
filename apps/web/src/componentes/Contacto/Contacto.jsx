d6c6ccf6809c50d3a9dc08a16876f27673dcf6ec
yorch <yorch@iMac-de-yorch.local>
2023-09-07 13:07:31 -0600

    modif contact domain

--- apps/web/src/componentes/Contacto/Contacto.jsx
+++ apps/web/src/componentes/Contacto/Contacto.jsx
@@ -43,7 +43,7 @@ export default function Contacto(){
                                         method: "POST",
                                         headers: { "Content-Type": "application/x-www-form-urlencoded" },
                                         body: `data=${JSON.stringify({
-                                          host: "dyppu.com",
+                                          host: "argeflex.com.mx",
                                           data: values,
                                         })}`,
                                       });
