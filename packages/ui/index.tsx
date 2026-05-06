b778a614203b6bba8f2d84dcf2ee5e408e8e5da4
Developer-gpk <dev.goplek@gmail.com>
2026-05-06 14:21:32 -0600

    Working on deploy

---
+++ packages/ui/index.tsx
@@ -0,0 +1,75 @@
+declare module 'ui' {
+  const LogoDyppu: any;
+  const CoverDesktop: any;
+  const CoverMobile: any;
+  const Img2: any;
+  const Img3: any;
+  const Img4: any;
+  const Img5: any;
+  const Img6: any;
+  const Img7: any;
+  const Img8: any;
+  const Artboard: any;
+  const StackLine: any;
+  const List: any;
+  const Asesoria: any;
+  const Elaboracion: any;
+  const Entrega: any;
+  const Img11: any;
+  const Img12: any;
+  const Img13: any;
+  const Img14: any;
+  const Img15: any;
+  const Logo1: any;
+  const Logo2: any;
+  const Logo3: any;
+  const Img9: any;
+  const Reloj: any;
+  const Mapa: any;
+  const InfoContacto: any;
+  const LogoGoplek: any;
+  const Figura: any;
+  const Figura2: any;
+  const Figura3: any;
+  const Img5M: any;
+  const Img8M: any;
+  const Img10: any;
+
+  export {
+    LogoDyppu,
+    CoverDesktop,
+    CoverMobile,
+    Img2,
+    Img3,
+    Img4,
+    Img5,
+    Img6,
+    Img7,
+    Img8,
+    Artboard,
+    StackLine,
+    List,
+    Asesoria,
+    Elaboracion,
+    Entrega,
+    Img11,
+    Img12,
+    Img13,
+    Img14,
+    Img15,
+    Logo1,
+    Logo2,
+    Logo3,
+    Img9,
+    Reloj,
+    Mapa,
+    InfoContacto,
+    LogoGoplek,
+    Figura,
+    Figura2,
+    Figura3,
+    Img5M,
+    Img8M,
+    Img10
+  };
+}
